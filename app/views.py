7ae5e865fa183aec7ac3d8e8bdb6d90500450e50
Kai N.B. McPheeters <kaimcpheeters@gmail.com>
2023-09-22 00:46:41 -0400

    Add some print logging

--- app/views.py
+++ app/views.py
@@ -5,15 +5,18 @@ import django_rq
 
 class TestMainViewSet(viewsets.ViewSet):
     def list(self, request):
+        print('Sending an event with value: main')
         data = {'message': 'Sending a event with value: main'}
         django_eventstream.send_event('test', 'message', 'main')
         return Response(data)
 
 def test_job():
+    print('Sending an event with value: worker')
     django_eventstream.send_event('test', 'message', 'worker')
 
 class TestWorkerViewSet(viewsets.ViewSet):
     def list(self, request):
+        print('Will send an event with value: worker')
         queue = django_rq.get_queue('default')
         queue.enqueue(test_job)
         data = {'message': 'Sending a event with value: worker'}
